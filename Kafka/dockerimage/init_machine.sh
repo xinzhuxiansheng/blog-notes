10963a3f746ccd7ad66c4cbd233172c17c81a03a
阿洋 <xinzhuxiansheng@126.com>
2021-10-29 03:17:46 +0800

    add

--- Kafka/dockerimage/init_machine.sh
+++ Kafka/dockerimage/init_machine.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-/usr/bin/wget http://containerfiles.corpautohome.com/ci-config/kafka/kafka_2.12-2.2.1.tar.gz -N -P /data/sysdir/servers/
+/usr/bin/wget http://containerfiles.corpxxxxx.com/ci-config/kafka/kafka_2.12-2.2.1.tar.gz -N -P /data/sysdir/servers/
 
 /usr/bin/tar zxvf /data/sysdir/servers/kafka_2.12-2.2.1.tar.gz -C /data/sysdir/servers/
 
 chmod 777 -R /data/sysdir/servers/kafka_2.12-2.2.1/bin/*.sh
 
-/usr/bin/wget http://containerfiles.corpautohome.com/ci-config/kafka/server_nginx_test.properties -N -P /data/sysdir/servers/kafka_2.12-2.2.1/config/
+/usr/bin/wget http://containerfiles.corpxxxxx.com/ci-config/kafka/server_nginx_test.properties -N -P /data/sysdir/servers/kafka_2.12-2.2.1/config/
 
 CustomTagIFconfig=`/usr/sbin/ifconfig eth0 | grep inet | grep netmask | grep broadcast | grep 10 | awk -F " " '{print $2}'`
 sed -i "s/custom-tag-ifconfig/$CustomTagIFconfig/g" /data/sysdir/servers/kafka_2.12-2.2.1/config/server_nginx_test.properties
